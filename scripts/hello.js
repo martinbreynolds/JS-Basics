2bf7bdee8d15803b325c4948badaa62e3fd24912
Martin Reynolds <martinandtoni@btinternet.com>
2021-02-25 21:17:20 +0000

    Initial Commit

---
+++ scripts/hello.js
@@ -0,0 +1,29 @@
+var myName = 'Martin';
+
+//A short one line comment
+
+alert(myName);
+console.log(myName);
+
+/* This is a multiline comment
+that goes over
+multiple pages*/
+
+document.write(myName);
+
+var a = 15;
+var b = 6;
+
+console.log('my numbers are', a, 'and', b)
+
+console.log('addition', a+b);
+console.log('subtraction', a-b);
+console.log('multiplication', a*b);
+console.log('division', a/b);
+console.log('modulas', a%b);
+
+var fName = 'Martin';
+var lName = 'Reynolds';
+
+console.log('Simple concactenate', fName+' '+lName);
+console.log(`String interprelation ${fName +' ' + lName}`)
